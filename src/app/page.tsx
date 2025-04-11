1c6b3584e024525ea4cc2a8401756ad73287a955
Pablo <pablonegociosnet@gmail.com>
2025-04-11 17:39:13 -0300

    font: adicionado a fonte do manual da marca

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
    <h1
-    className="font-black text-3xl text-center text-blue-500 bg-red-500 p-4 m-4 rounded-lg shadow-lg"
+    className="font-henderson-black text-5xl text-center text-gray-900" 
    >
-    Bora bora!
+    Comunidade Ministério escolhidos
    </h1>
   );
 }
